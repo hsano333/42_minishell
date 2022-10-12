092864523908fb23da063e2b0a64fd671763a72f
hsano <hsano@student.42tokyo.jp>
2022-10-12 13:58:37 +0900

    add parser file

---
+++ include/parser.h
@@ -0,0 +1,22 @@
+
+#ifndef CMD_H
+# define CMD_H
+
+#include "lexer.h"
+#include "stdlib.h"
+
+enum e_fd
+{
+	STD_IN = 0,
+	STD_OUT,
+	STD_ERR,
+	FD_PIPE,
+};
+typedef enum e_fd t_fd;
+typedef struct s_cmd {
+	t_fd	in;
+	char	**cmd;
+	t_fd	out;
+	size_t	no;
+}	t_cmd;
+#endif
