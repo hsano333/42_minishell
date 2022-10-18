b98ee32d3d14f80bcbb1fbf5c34ec8798a714330
hsano <hsano@student.42tokyo.jp>
2022-10-19 00:16:09 +0900

    fix expanding $

--- srcs/parser_expand_doller.c
+++ srcs/parser_expand_doller.c
@@ -6,7 +6,7 @@
 /*   By: hsano <hsano@student.42tokyo.jp>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/12 14:04:16 by hsano             #+#    #+#             */
-/*   Updated: 2022/10/15 23:23:35 by hsano            ###   ########.fr       */
+/*   Updated: 2022/10/19 00:08:58 by hsano            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,6 +15,18 @@
 #include "libft_str.h"
 #include "minishell.h"
 #include "lexer_util.h"
+
+static void	expand_recursive(char *str, token_type pre_token, char **dst_str);
+
+static int	is_valid_env_char(char c)
+{
+	if (ft_isalnum(c))
+		return (true);
+	else if (c == '_')
+		return (true);
+	return (false);
+
+}
 
 static char	*join_and_free_str(char *dst, char *str, int i, int is_free_str)
 {
@@ -35,24 +47,38 @@ static char	*join_and_free_str(char *dst, char *str, int i, int is_free_str)
 	return (concat_str);
 }
 
+static void expand(char *str, char **dst_str, size_t i)
+{
+	char	*env_str;
+	size_t	j;
+	char	tmp;
+
+	j = 0;
+	while (is_valid_env_char(str[j]))
+			j++;
+	tmp = str[j];
+	str[j] = '\0';
+	env_str = env_func(NULL, GET_ENV, str, NULL);
+	str[j] = tmp;
+	printf("str=%s, env_str=%s\n", str, env_str);
+	*dst_str = join_and_free_str(*dst_str, env_str, ft_strlen(env_str), true);
+	if (str[i] == '$' && is_valid_env_char(str[i + 1]))
+		expand_recursive(&(str[i + 1]), DOLLER, dst_str);
+	else if (str[i])
+		expand_recursive(&(str[i]), NON, dst_str);
+
+}
+
 static void	expand_recursive(char *str, token_type pre_token, char **dst_str)
 {
 	size_t	i;
-	char	*env_str;
 
 	i = 0;
-	while (str[i] && ft_isalnum(str[i]))
+	while (str[i] && is_valid_env_char(str[i]))
 		i++;
 	if (pre_token == DOLLER && i > 0)
-	{
-		env_str = env_func(NULL, GET_ENV, str, NULL);
-		*dst_str = join_and_free_str(*dst_str, env_str, ft_strlen(env_str), true);
-		if (str[i] == '$' && ft_isalnum(str[i + 1]))
-			expand_recursive(&(str[i + 1]), DOLLER, dst_str);
-		else if (str[i])
-			expand_recursive(&(str[i]), NON, dst_str);
-	}
-	else if(str[i] == '$' && ft_isalnum(str[i + 1]))
+		expand(str, dst_str, i);
+	else if(str[i] == '$' && is_valid_env_char(str[i + 1]))
 	{
 		*dst_str = join_and_free_str(*dst_str, str, i, false);
 		expand_recursive(&(str[i + 1]), DOLLER, dst_str);
