d581ce241bf915ffb273acd78b6464932fa293e3
hsano <hsano@student.42tokyo.jp>
2022-11-14 02:22:29 +0900

    fixed redirect for buildin command

--- srcs/exe_cmds.c
+++ srcs/exe_cmds.c
@@ -6,7 +6,7 @@
 /*   By: hsano <hsano@student.42tokyo.jp>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/24 16:55:41 by hsano             #+#    #+#             */
-/*   Updated: 2022/11/13 12:42:57 by hsano            ###   ########.fr       */
+/*   Updated: 2022/11/14 02:18:34 by hsano            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -28,27 +28,70 @@ int	is_continue(t_cmds *cmds, int rval, size_t i)
 	return (false);
 }
 
-static int	change_buidin_fd(t_pipe *pipe, int back)
+static int	change_buildin_fd_in(t_pipe *pipe, int *fd)
+{
+	*fd = 0;
+	if (pipe->in_file)
+	{
+		close(*fd);
+		*fd = open(pipe->in_file, O_RDONLY);
+		if (fd < 0)
+		{
+			ft_putstr_fd("open error:", 2);
+			write(2, pipe->in_file, ft_strlen(pipe->in_file)); 
+			write(2, "\n", 1);
+			return (false);
+		}
+	}
+	if (dup2(*fd, pipe->option_fd_in) == -1)
+		return (false);
+	return (true);
+}
+
+static int	change_buildin_fd_out(t_pipe *pipe, int *fd)
+{
+	*fd = pipe->option_fd_out;
+	if (pipe->out_file)
+	{
+		close(*fd);
+		*fd = open(pipe->out_file, (O_APPEND | O_WRONLY));
+		if (*fd < 0)
+		{
+			ft_putstr_fd("open error:", 2);
+			write(2, pipe->out_file, ft_strlen(pipe->out_file)); 
+			write(2, "\n", 1);
+			return (false);
+		}
+	}
+	if (dup2(*fd, pipe->option_fd_out) == -1)
+		return (false);
+	return (true);
+
+}
+
+static int	change_buildin_fd(t_pipe *pipe, int back)
 {
 	static int	pre_fd_in = 0;
 	static int	pre_fd_out = 1;
+	static int	fd_in = 0;
+	static int	fd_out = 1;
 
-	if (back == false && pipe->option_fd_in != 0)
+	if (back == false)
 	{
-		pre_fd_in = dup(0);
-		close(0);
-		dup2(pipe->option_fd_in, 0);
+		pre_fd_in = dup(pipe->option_fd_in);
+		pre_fd_out = dup(pipe->option_fd_out);
+		change_buildin_fd_in(pipe, &fd_in);
+		change_buildin_fd_out(pipe, &fd_out);
 	}
-	else if (back == true)
-		dup2(0, pipe->option_fd_in);
-	if (back == false && pipe->option_fd_out != 0)
+	if (back == true)
 	{
-		pre_fd_out = dup(0);
-		close(0);
-		dup2(pipe->option_fd_out, 1);
+		if (fd_out != 1)
+			close(fd_out);
+		if (fd_in != 1)
+			close(fd_in);
+		dup2(pre_fd_in, 0);
+		dup2(pre_fd_out, 1);
 	}
-	else if (back == true)
-		dup2(1, pipe->option_fd_out);
 	return (true);
 }
 
@@ -71,10 +114,12 @@ void	exe_cmds(t_cmds *cmds)
 		if (cmds[i].len > 1 || (cmds[i].len == 1 \
 					&& !cmds[i].pipes[0].is_builtin_cmd))
 			rval = pipex(&(cmds[i]), envv);
-		else if (change_buidin_fd(&(cmds[i].pipes[0]), false))
+		else if (change_buildin_fd(&(cmds[i].pipes[0]), false))
+		{
 			rval = exec_builtin_cmd(cmds[i].pipes[0].param);
+			change_buildin_fd(&(cmds[i].pipes[0]), true);
+		}
 		handle_global_signals();
-		change_buidin_fd(&(cmds[i].pipes[0]), true);
 		set_exit_status(rval);
 		i++;
 	}
