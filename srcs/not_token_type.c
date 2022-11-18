ca88d1ca96babddd50c79b7480fe997568a213ab
hsano <hsano@student.42tokyo.jp>
2022-11-19 03:24:22 +0900

    foreget to upload file

---
+++ srcs/not_token_type.c
@@ -0,0 +1,55 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   not_token_type.c                                   :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: hsano <hsano@student.42tokyo.jp>           +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/11/17 14:43:57 by hsano             #+#    #+#             */
+/*   Updated: 2022/11/19 02:26:04 by hsano            ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "token_type.h"
+#include <stdio.h>
+
+int	is_begin_error_token(t_token_type type)
+{
+	const t_token_type	error_token[] = {PIPE, D_PIPE, D_AMPERSAND, RPAREN};
+	size_t				i;
+	size_t				len;
+
+	i = 0;
+	len = sizeof(error_token) / sizeof(t_token_type);
+	while (i < len)
+	{
+		if (type == error_token[i])
+			return (true);
+		i++;
+	}
+	return (false);
+}
+
+int	is_subshell_begin_error_token(t_token_type type)
+{
+	const t_token_type	error_token[] = {PIPE, D_PIPE, D_AMPERSAND, RPAREN};
+	size_t				i;
+	size_t				len;
+
+	i = 0;
+	len = sizeof(error_token) / sizeof(t_token_type);
+	while (i < len)
+	{
+		if (type == error_token[i])
+			return (true);
+		i++;
+	}
+	return (false);
+}
+
+int	not_if_token(t_token_type type)
+{
+	if (type == D_AMPERSAND || type == D_PIPE)
+		return (false);
+	return (true);
+}
