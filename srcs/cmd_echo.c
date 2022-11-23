62a241c1a39ce75735b994223e6fa75228bb13a9
Masanori Aoyagi <maoyagi@c1r1s1.42tokyo.jp>
2022-11-23 19:25:16 +0900

    echo adjustment

--- srcs/cmd_echo.c
+++ srcs/cmd_echo.c
@@ -6,7 +6,7 @@
 /*   By: maoyagi <maoyagi@student.42tokyo.jp>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/26 08:33:34 by maoyagi           #+#    #+#             */
-/*   Updated: 2022/11/23 18:18:11 by maoyagi          ###   ########.fr       */
+/*   Updated: 2022/11/23 19:24:26 by maoyagi          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,7 +19,7 @@ int	cmd_echo(char **cmd)
 
 	print_newline = true;
 	i = 1;
-	while (cmd[i] && ft_strncmp(cmd[i], "-n", ft_strlen("-n")) == 0)
+	while (cmd[i] && ft_strncmp(cmd[i], "-n", ft_strlen(cmd[i])) == 0)
 	{
 		print_newline = false;
 		i++;
