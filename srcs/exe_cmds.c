dd34153291892cf099c8980896aa7b1b3c674c5a
Hiroaki Sano <hsano@student.42tokyo.jp>
2022-11-30 02:43:34 +0900

    fixed exit status in parser error

--- srcs/exe_cmds.c
+++ srcs/exe_cmds.c
@@ -6,7 +6,7 @@
 /*   By: hsano <hsano@student.42tokyo.jp>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/24 16:55:41 by hsano             #+#    #+#             */
-/*   Updated: 2022/11/30 00:53:36 by hsano            ###   ########.fr       */
+/*   Updated: 2022/11/30 02:41:49 by hsano            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -75,6 +75,7 @@ int	exe_cmds(t_token *tokens)
 	while (tokens[i].type != EOS)
 	{
 		cmds = get_cmds(tokens, rval, &i, &type);
+		rval = 1;
 		handle_ign_signals();
 		if (cmds && (cmds[0].len == 1 && cmds[0].pipes[0].is_builtin_cmd))
 			rval = builtin_wrapper(cmds[0].pipes[0].param, &(cmds[0].pipes[0]));
--- srcs/parser_check_valid_file.c
+++ srcs/parser_check_valid_file.c
@@ -6,12 +6,13 @@
 /*   By: hsano </var/mail/hsano>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/29 02:25:12 by hsano             #+#    #+#             */
-/*   Updated: 2022/11/29 03:14:37 by hsano            ###   ########.fr       */
+/*   Updated: 2022/11/30 02:02:54 by hsano            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "parser_check_valid_file.h"
 #include "libft_put.h"
+#include "exit_status.h"
 #include <stdio.h>
 
 int	parser_check_valid_file(t_token *token)
@@ -23,6 +24,7 @@ int	parser_check_valid_file(t_token *token)
 	{
 		if (ft_strchr(token->literal, PARSER_DELIMITER))
 		{
+			set_exit_status(258);
 			ft_putstr_fd("minishell: ambiguous redirect\n", 2);
 			rval = false;
 			token->type = NON;
